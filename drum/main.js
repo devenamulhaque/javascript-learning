cdde2c4d8b819c46e4d80807e6e499c75b9cd004
rearmustak <enamul.iu91@gmail.com>
2018-02-14 09:34:50 +0600

    drum js file changed

--- drum/main.js
+++ drum/main.js
@@ -1,9 +1,15 @@
 window.addEventListener('keydown', function (e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-	if(!audio) return // if audio is not found
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; // If auto is not found
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
-	console.log(e);
 });
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+	this.classList.remove('playing');
+}
