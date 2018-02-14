c002de828e13df50b6af27a0b2791c028226ad46
rearmustak <enamul.iu91@gmail.com>
2018-02-14 15:18:27 +0600

    clock file added

--- clock/main.js
+++ clock/main.js
@@ -3,7 +3,6 @@
 var hourHand = document.querySelector('.hour-hand');
 var minHand = document.querySelector('.min-hand');
 var secHand = document.querySelector('.sec-hand');
-var digitalClock = document.querySelector('.digital-clock');
 
 
 setInterval(runningTime, 1000);
@@ -20,16 +19,6 @@ function runningTime() {
 	minHand.style.setProperty('transform', `rotate(${(min * 6) + 90}deg)`);
 	hourHand.style.setProperty('transform', `rotate(${(hour * 30) + 92}deg)`);
 
-
-
-
-	// Digital Clock
-	hour = hour % 12;
-	hour = hour < 10 ? `0${hour}`: hour;
-	min = min < 10 ? `0${min}`: min;
-	sec = sec < 10 ? `0${sec}`: sec;
-
-	// digitalClock.textContent = `Time is ${hour}:${min}:${sec}`;
 }
 
 
